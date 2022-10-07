a23d1fdff963e1011f7bcbcbe086b2eb5089cb6f
kate-kravchuk <106491612+kate-kravchuk@users.noreply.github.com>
2022-10-07 14:02:15 +0200

    Added links on git and netlify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,30 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather Forecast</div>;
+  return (
+    <div className="App">
+      Weather Forecast
+      <footer>
+        This application is created by Kateryna Kravchuk with the guidance of
+        Matt Delac. It is
+        <a
+          href="https://github.com/kate-kravchuk/react-weather-forecast"
+          target="_blank"
+        >
+          {" "}
+          open sourced on GitHub
+        </a>{" "}
+        and
+        <a
+          href="https://aesthetic-stroopwafel-a4cb5d.netlify.app/"
+          target="_blank"
+        >
+          {" "}
+          deployed on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
